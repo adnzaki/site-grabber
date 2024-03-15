11d52ce774187da80d52a0fb9fd5ac3e0f9ec4c8
Adnan Zaki <adnzaki@outlook.com>
2024-03-15 09:31:26 +0700

    Update links

--- app/Models/SiteListModel.php
+++ app/Models/SiteListModel.php
@@ -4,13 +4,13 @@ class SiteListModel
 {
     public $siteList = [
         'erapor-pgn7'   => [
-            'url'   => 'https://676a-103-119-138-249.ngrok-free.app/login.php',
+            'url'   => 'https://9225-101-255-168-6.ngrok-free.app/login.php',
             'title' => 'eRapor SDN Pengasinan VII',
             'paid'  => true,
             'exp'   => '1724216077'
         ],
         'dapodik-pgn7'  => [
-            'url'   => 'https://2486-103-119-138-249.ngrok-free.app',
+            'url'   => 'https://09cd-101-255-168-6.ngrok-free.app/',
             'title' => 'Dapodik SDN Pengasinan VII',
             'paid'  => true,
             'exp'   => '1724216077'
